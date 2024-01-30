6891152f6ef19f9b8590929768cd161e759a8871
Niko Mastakas <nicolasmastakas@gmail.com>
2024-01-30 20:04:32 -0300

    feat: update Destinations Page

--- src/screens/Destinations/Destinations.tsx
+++ src/screens/Destinations/Destinations.tsx
@@ -1,52 +1,28 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView, ScrollView } from 'react-native'
-import { useSelector, useDispatch } from 'react-redux'
+import { FlatList } from 'react-native'
+import { useSelector } from 'react-redux'
 import { StackNavigationProp } from '@react-navigation/stack'
-import { useGetDestinationsQuery } from '../../api/services'
-import { setSelectedDestinationId } from '../../features/destinations'
 import { TopNavigator } from '../../navigation/navigators/TopNavigator'
 import { RootStackParamList } from '../../types'
-import { DestinationCard } from '../../components/Card/Card'
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  scrollView: {
-    width: '100%',
-  },
-})
+import { DestinationCard } from '../../components/DestinationCard'
+import { selectTopLevelDestinations } from '../../features/destinations/destionationsSelectors'
+import ScreenWrapper from '../../components/ScreenWrapper'
 
 type DestinationsProps = {
   navigation: StackNavigationProp<RootStackParamList, 'Destinations'>
 }
 
 export const Destinations = ({ navigation }: DestinationsProps) => {
-  const { data, error, isLoading } = useGetDestinationsQuery()
-  const dispatch = useDispatch()
-  const selectedDestinationId = useSelector((state) => state.destinations.selectedDestinationId)
-
-  if (isLoading) return <Text>Cargando...</Text>
-  if (error) return <Text>Error al cargar los destinos</Text>
-
-  const handleSelectDestination = (id) => {
-    dispatch(setSelectedDestinationId(id))
-  }
+  const topLevelDestinations = useSelector(selectTopLevelDestinations)
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <ScreenWrapper>
       <TopNavigator title="Destinos" navigation={navigation} />
-      <ScrollView style={styles.scrollView}>
-        <View style={styles.container}>
-          {data?.data.map((destination) => (
-            <DestinationCard
-              key={destination.id}
-              destination={destination}
-              onSeeMorePress={() => handleSelectDestination(destination.id)}
-            />
-          ))}
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <FlatList
+        data={topLevelDestinations}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <DestinationCard destination={item} onSeeMorePress={() => {}} />}
+      />
+    </ScreenWrapper>
   )
 }
